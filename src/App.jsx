df39aaafa128bbe1cd88aebb203eea6f5be872ec
https://github.com/Shreejad123 <shreejadkotian00@gmail.com>
2026-05-29 10:11:36 +0530

    Updated the landing page

--- src/App.jsx
+++ src/App.jsx
@@ -7,13 +7,11 @@ import styles from "./App.module.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { FaReact } from "react-icons/fa";
 import { IoLogoJavascript } from "react-icons/io";
-import { FaHtml5 } from "react-icons/fa";
-import { FaCss3Alt } from "react-icons/fa6";
-import { FaGithub } from "react-icons/fa6";
-import { FaLinkedin } from "react-icons/fa";
-import { FaPhone } from "react-icons/fa6";
-import { MdEmail } from "react-icons/md";
+import { FaBootstrap } from "react-icons/fa";
+import { RiNextjsFill } from "react-icons/ri";
+
 import Projects from "./components/projects";
+import { BsTypescript } from "react-icons/bs";
 function App() {
   return (
     <>
@@ -46,23 +44,33 @@ function App() {
                   marginBottom: 10,
                 }}
               />
-              <FaHtml5
+              <FaBootstrap
                 style={{
                   fontSize: 30,
-                  color: "#FF4500",
+                  color: "#7952B3",
                   marginLeft: 20,
                   marginTop: 10,
                   marginRight: 3,
                   marginBottom: 10,
                 }}
               />{" "}
-              <FaCss3Alt
+              <RiNextjsFill
                 style={{
                   fontSize: 40,
                   marginRight: 3,
                   marginLeft: 20,
                   marginTop: 10,
-                  color: "#1E90FF",
+                  color: "white",
+                  marginBottom: 10,
+                }}
+              />
+              <BsTypescript
+                style={{
+                  fontSize: 36,
+                  marginRight: 3,
+                  marginLeft: 20,
+                  marginTop: 10,
+                  color: "#3178C6",
                   marginBottom: 10,
                 }}
               />
@@ -70,31 +78,10 @@ function App() {
             <div className={styles.techStack}>
               <span className={styles.tech}>React js</span>
               <span className={styles.tech}>Javascript</span>
-              <span className={styles.tech}>HTML</span>
-              <span className={styles.tech}>CSS</span>
+              <span className={styles.tech}>Bootstrap</span>
+              <span className={styles.tech}>Nextjs</span>
+              <span className={styles.tech}>TypeScript</span>
             </div>
-          </div>
-          <div className={styles.comunicationContainer}>
-            <a
-              href="https://github.com/Shreejad123"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <FaGithub size={20} className={styles.comunication} />
-            </a>
-            <a
-              href="https://www.linkedin.com/in/shreeja-d-kotian-b4639a286/"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <FaLinkedin size={20} className={styles.comunication} />
-            </a>
-            <a href="tel:+91 9483350641">
-              <FaPhone className={styles.comunication} />
-            </a>
-            <a href="mailto:shreejadkotian00@gmail.com">
-              <MdEmail size={20} className={styles.comunication} />
-            </a>
           </div>
         </div>
       </div>
--- src/components/home.jsx
+++ src/components/home.jsx
@@ -4,6 +4,10 @@ import { useNavigate } from "react-router-dom";
 import { FaRegCheckCircle } from "react-icons/fa";
 import { FaArrowRight } from "react-icons/fa6";
 import { PiHandWavingDuotone } from "react-icons/pi";
+import { FaGithub } from "react-icons/fa6";
+import { FaLinkedin } from "react-icons/fa";
+import { FaPhone } from "react-icons/fa6";
+import { MdEmail } from "react-icons/md";
 function Home() {
   const navigate = useNavigate();
   return (
@@ -13,9 +17,7 @@ function Home() {
           <PiHandWavingDuotone className={styles.handicon} /> Hi, &nbsp;I'm
         </p>
         <h3 className={styles.header}>
-          <b>
-            <br></br>Shreeja D Kotian
-          </b>
+          <b>Shreeja D Kotian</b>
         </h3>
         <p className={styles.paragraph}>
           <b>Frontend Web Developer</b>
@@ -39,6 +41,28 @@ function Home() {
           >
             View my Projects
           </button>
+        </div>
+        <div className={styles.comunicationContainer}>
+          <a
+            href="https://github.com/Shreejad123"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <FaGithub size={20} className={styles.comunication} />
+          </a>
+          <a
+            href="https://www.linkedin.com/in/shreeja-d-kotian-b4639a286/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <FaLinkedin size={20} className={styles.comunication} />
+          </a>
+          <a href="tel:+91 9483350641">
+            <FaPhone className={styles.comunication} />
+          </a>
+          <a href="mailto:shreejadkotian00@gmail.com">
+            <MdEmail size={20} className={styles.comunication} />
+          </a>
         </div>
       </div>
     </>
