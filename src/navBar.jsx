674b86a79faf8001f9e91656c69c1f03736593fc
https://github.com/Shreejad123 <shreejadkotian00@gmail.com>
2026-06-01 22:57:59 +0530

    made navbar smartphone responsive

--- src/navBar.jsx
+++ src/navBar.jsx
@@ -8,32 +8,88 @@ import Projects from "./components/project";
 import ThemeToggleButton from "./components/themeTogglebutton";
 import ExampleDoc from "../src/assets/Shreeja D Kotian_resume.pdf";
 import { LuDownload } from "react-icons/lu";
+import { useState } from "react";
+import CloseMenu from "./assets/close.svg";
+import MenuIcon from "./assets/menu.svg";
 function NavBar() {
+  const [click, setClick] = useState(false);
+  const handleClick = () => setClick(!click);
+  const closeMobileMenu = () => setClick(false);
   const navigate = useNavigate();
   return (
     <>
-      <p className={styles.iconsContainer}>
-        <span className={styles.icons} onClick={() => navigate("/")}>
-          Home
-        </span>
-        <span className={styles.icons} onClick={() => navigate("/projects")}>
-          Projects
-        </span>
-        <span className={styles.icons} onClick={() => navigate("/skills")}>
-          Skills
-        </span>
-        <span className={styles.icons} onClick={() => navigate("/about")}>
-          About me
-        </span>
-        <a href={ExampleDoc} download="Shreeja_D_Kotian_resume" target="_blank">
-          <button className={`btn btn-info ${styles.resume}`}>
-            <LuDownload /> &nbsp; Download resume
-          </button>
-        </a>
-        <span className={styles.theme}>
-          <ThemeToggleButton />
-        </span>
-      </p>
+      <div className={styles.iconsContainer}>
+        <ul className={`${styles.navOptions} ${click ? styles.active : ""}`}>
+          <li>
+            <span
+              className={styles.icons}
+              onClick={() => {
+                navigate("/");
+                closeMobileMenu();
+              }}
+            >
+              Home
+            </span>
+          </li>
+          <li>
+            <span
+              className={styles.icons}
+              onClick={() => {
+                navigate("/projects");
+                closeMobileMenu();
+              }}
+            >
+              Projects
+            </span>
+          </li>
+          <li>
+            <span
+              className={styles.icons}
+              onClick={() => {
+                navigate("/skills");
+                closeMobileMenu();
+              }}
+            >
+              Skills
+            </span>
+          </li>
+          <li>
+            <span
+              className={styles.icons}
+              onClick={() => {
+                navigate("/about");
+                closeMobileMenu();
+              }}
+            >
+              About me
+            </span>
+          </li>
+
+          <li>
+            <a
+              href={ExampleDoc}
+              download="Shreeja_D_Kotian_resume"
+              target="_blank"
+            >
+              <button className={`btn btn-info ${styles.resume}`}>
+                <LuDownload /> &nbsp; Download resume
+              </button>
+            </a>
+          </li>
+          <li>
+            <span className={styles.icons}>
+              <ThemeToggleButton />
+            </span>
+          </li>
+        </ul>
+      </div>
+      <div className={styles.mobileMenu} onClick={handleClick}>
+        <img
+          src={click ? CloseMenu : MenuIcon}
+          alt="menu"
+          className={styles.menuIcon}
+        />
+      </div>
     </>
   );
 }
